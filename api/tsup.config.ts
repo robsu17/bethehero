d8087e6f94293ec6003f618241e660f5725f555c
Robson Wendel <robson.costa134@gmail.com>
2023-07-30 09:11:39 -0300

    update tsup

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -1,5 +1,6 @@
+// tsup.config.js
 export default {
-  entryPoints: ['src/server.ts'],
-  outDir: 'build',
-  format: 'esm', // Formato de módulo ES
+  entryPoints: ["src/server.ts"], // Ponto de entrada do seu projeto TypeScript
+  outDir: "build", // Pasta de saída para os arquivos JavaScript
+  format: "cjs", // Formato de módulo para o bundle (CommonJS)
 };
