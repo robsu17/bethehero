ecd7633b78238c6a100aa8009df19ec32c6c595d
Robson Wendel <robson.costa134@gmail.com>
2023-07-30 10:33:28 -0300

    update routes

--- api/src/env/index.ts
+++ api/src/env/index.ts
@@ -1,15 +1,8 @@
-import { config } from "dotenv";
+import "dotenv";
 
 import { z } from "zod";
 
-if (process.env.NODE_ENV === "test") {
-  config({ path: ".env.test" });
-} else {
-  config();
-}
-
 const envSchema = z.object({
-  DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
